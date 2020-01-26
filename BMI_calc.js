311637e4e625a11fd76a4352659161c2946f5f3e
Jaime <jaime.cruz.hostalot@gmail.com>
2020-01-26 14:50:28 +0100

    Project start

---
+++ BMI_calc.js
@@ -0,0 +1,19 @@
+
+
+//BMI Calculator 1 multiply height by height, after divide by (kilograms % Cacl_heigh)
+// then compare in the status table.
+
+
+// Imperial methode. 
+// Weight in pounds by 703  // height in inch x Height(inch) 
+// divide answerd 1 by answerd 2 = BMI
+
+
+// Table:
+// underweight <18.5
+// normal  18.5 to 24.9
+// overW    25-29.9
+// Obesity  30 - 34.9
+// obesity 2    35 - 39.9
+// Extreme Obe  40
+
