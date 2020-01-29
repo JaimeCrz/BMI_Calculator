1d1bc9fd2dceba1ddc7e53b0b94080fd3753c0ef
Jaime <jaime.cruz.hostalot@gmail.com>
2020-01-29 22:33:23 +0100

    Added Metric BMI, updated Index

--- src/assets/js/bmiCalculator.js
+++ src/assets/js/bmiCalculator.js
@@ -15,3 +15,8 @@
 // obesity 2    35 - 39.9
 // Extreme Obe  40
 
+const  calculateMetricBMI = (weight, height) => {
+	let  result
+	result = (weight / ((height / 100) * (height / 100))).toFixed(2)
+	return  result
+}
