e48a5ea1eccc3383fec6a767f1ebbd513aec546f
Jaime <jaime.cruz.hostalot@gmail.com>
2020-01-29 17:32:43 +0100

    updated: Index.html//Added button + input //updated:Bmifeature.js // To check parameters

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,6 +1,10 @@
 describe('User can check their Bmi', () => {
 	it('user can enter weight and height', () => {
 		cy.visit('http://localhost:3001')
+		cy.contains('Your BMI Calculator')
+		cy.get('#weight').type(90)
+		cy.get('#height').type('186')
+		cy.get('#Calculate').click()
 	})
 })
 
@@ -8,5 +12,8 @@ describe('User can check their Bmi', () => {
 	it('user can enter weight and height', () => {
 		cy.visit('http://localhost:3001')
 		cy.contains('Your BMI Calculator')
+		cy.get ('#weight').type(90)
+		cy.get('#height').type('186')
+		cy.get('#Calculate').click()
 	})
 })
